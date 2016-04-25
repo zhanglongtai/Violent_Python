add2dedc6c4940448c1b10668009666ff017a68e
zhanglongtai <neo_seele@hotmail.com>
2016-04-25 18:43:50 +0800

    update

--- Chapter_03/skype-parse.py
+++ Chapter_03/skype-parse.py
@@ -13,6 +13,22 @@ def printProfile(skypeDB):
         print '[+] Profile Data: ' + str(row[4])
 
 
+def printContacts(skypeDB):
+    conn = sqlite3.connect(skypeDB)
+    c = conn.cursor()
+    c.execute("SELECT displayname, skypename, city, country, phone_mobile, birthday FROM Contacts;")
+    for row in c:
+        print '[*] -- Found Contact --'
+        print '[+] User: ' + str(row[0])
+        print '[+] Skype Username: ' + str(row[1])
+        if str(row[2]) != '' and str(row[2]) != 'None':
+            print '[+] Location: ' + str(row[2]) + ', ' + str(row[3])
+        if str(row[4]) != 'None':
+            print '[+] Mobile Number :' + str(row[4])
+        if str(row[5]) != 'None':
+            print '[+] Mobile Number :' + str(row[5])
+
+
 def main():
     skypeDB = "main.db"
     printProfile(skypeDB)
